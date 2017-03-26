05763df551c062479178cecaf4532d0250ba355b
soundgnome <terran@halfcadence.net>
2017-03-25 19:11:09 -0700

    refining hexmap

--- src/scripts/config.gd
+++ src/scripts/config.gd
@@ -2,5 +2,6 @@ extends Node
 
 var map_boundaries = [0,0,32,32]
 var hex_size = 24
-var grid_color = Color(160, 160, 160)
+var map_offsets = [24, 24]
+var grid_color = Color(0.75, 0.75, 0.75)
 var star_color = Color(255, 255, 255)
--- src/scripts/world_map/map.gd
+++ src/scripts/world_map/map.gd
@@ -6,6 +6,8 @@ var pixel_offsets
 
 func _ready():
 	self.boundaries = CONFIG.map_boundaries
+	self.pixel_offsets = CONFIG.map_offsets
+
 	var size = CONFIG.hex_size
 	var side = size/sqrt(3)
 	self.hex_lengths = {
@@ -15,28 +17,50 @@ func _ready():
 		'halfside': int(side/2),
 		'rowheight': int(side*1.5),
 	}
-	self.pixel_offsets = [0, 0]
 
 func _draw():
 	for x in range(self.boundaries[0], self.boundaries[2]):
-		for y in range(self.boundaries[1], self.boundaries[3]):
-			self.draw_hex([x,y])
+		if (x == 0):
+			for y in range(self.boundaries[1], self.boundaries[3]-1):
+				self._draw_hex([x,y], [0,1,2,3,4])
+			self._draw_hex([x, self.boundaries[3]-1], [0,1,2,3,4,5])
+		else:
+			for y in range(self.boundaries[1], self.boundaries[3]-1):
+				self._draw_hex([x,y], [2,3,4])
+			self._draw_hex([x, self.boundaries[3]-1], [2,3,4,5,0])
 
-func draw_hex(pos):
-	var center = self.pos_to_pixel(pos)
-	var start = Vector2(center[0] - self.hex_lengths['halfwidth'], center[1] - self.hex_lengths['halfside'])
-	var end = Vector2(center[0], center[1] - self.hex_lengths['side'])
-	self.draw_line(start, end, CONFIG.grid_color, 1)
-	start = end
-	end.x += self.hex_lengths['halfwidth']
-	end.y += self.hex_lengths['halfside']
-	self.draw_line(start, end, CONFIG.grid_color, 1)
-	start = end
-	end.y += self.hex_lengths['side']
-	self.draw_line(start, end, CONFIG.grid_color, 1)
 
-func pos_to_pixel(pos):
+func _get_hex_corners(pos):
+	var center = self._pos_to_pixel(pos)
+	var xs = [
+		center[0] - self.hex_lengths['halfwidth'],
+		center[0],
+		center[0] + self.hex_lengths['halfwidth'],
+	]
+	var ys = [
+		center[1] - self.hex_lengths['side'],
+		center[1] - self.hex_lengths['halfside'],
+		center[1] + self.hex_lengths['halfside'],
+		center[1] + self.hex_lengths['side'],
+	]
+	var corners = [
+		Vector2(xs[1], ys[3]),
+		Vector2(xs[0], ys[2]),
+		Vector2(xs[0], ys[1]),
+		Vector2(xs[1], ys[0]),
+		Vector2(xs[2], ys[1]),
+		Vector2(xs[2], ys[2]),
+	]
+	return corners
+
+func _draw_hex(pos, sides):
+	var corners = self._get_hex_corners(pos)
+	for side in sides:
+		draw_line(corners[side], corners[(side+1)%6], CONFIG.grid_color)
+
+
+func _pos_to_pixel(pos):
 	var x = (self.hex_lengths['width'] * (pos[0] - self.boundaries[0])) \
-		+ (self.hex_lengths['halfwidth'] * (self.boundaries[3] - pos[1])) + self.pixel_offsets[0]
-	var y = (self.hex_lengths['rowheight'] * (self.boundaries[3] - pos[1])) + self.pixel_offsets[1]
+		+ (self.hex_lengths['halfwidth'] * pos[1]) + self.pixel_offsets[0]
+	var y = (self.hex_lengths['rowheight'] * pos[1]) + self.pixel_offsets[1]
 	return [x,y]
